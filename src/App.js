bdf62b81f1e225455bb9b328b0a36050c3d52234
Ghazi <hichri.ghaz1@gmail.com>
2022-08-08 12:46:48 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbarrrr from './Compnts/Navbarrrr';
+import Aboutme from './Compnts/Aboutme';
+import Projects from './Compnts/Projects';
+import Contact from './Compnts/Contact';
+import Footer from './Compnts/Footer';
+
+function App() {
+  return (
+   <div>
+   <Navbarrrr/>
+   <Aboutme/>
+   <Projects/>
+   <Contact/>
+   <Footer/>
+
+
+
+   </div>
+  );
+}
+
+export default App;
